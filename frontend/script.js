21185b7a6fc51d7b94166c8fbe6a200a3b627d52
Fabrizzio Fabiano Esquivel Mori <fabrizzio.esquivel@unas.edu.pe>
2024-11-15 11:25:14 -0500

    Cloud Fix 2

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 document.getElementById("fetch-data").addEventListener("click", async () => {
     const responseContainer = document.getElementById("response");
     try {
-        const response = await fetch("https://arn:aws:execute-api:us-east-2:676206932076:st56qzz7dk/*/*/{proxy+}/cloud");
+        const response = await fetch("https://st56qzz7dk.execute-api.us-east-2.amazonaws.com/cloud");
         const data = await response.json();
         responseContainer.textContent = data.message;
     } catch (error) {
